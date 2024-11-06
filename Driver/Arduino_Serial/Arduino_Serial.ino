8ce48f18465c48a452153f5ee5ba58c8f97c1a6c
Karina <karinamguerrero@estudiantec.cr>
2024-11-06 14:41:42 -0600

    Add gitignore

---
+++ Driver/Arduino_Serial/Arduino_Serial.ino
@@ -0,0 +1,19 @@
+#define LED 13
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if(Serial.available()){
+    char command = Serial.read();
+    if(command == '1'){
+      digitalWrite(LED, HIGH);
+    }
+    else if(command == '0'){
+      digitalWrite(LED, LOW);
+    }
+  }
+
+}
